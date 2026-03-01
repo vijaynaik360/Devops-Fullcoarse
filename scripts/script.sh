7afe8e7430ff5acecc490c442e87a132f0dd9595
Vijay Naik <vijaynaik.ae@gmail.com>
2026-03-01 20:23:06 +0530

    jenkins script uploaded

---
+++ scripts/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre -y
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
